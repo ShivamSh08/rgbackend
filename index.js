51f68fb3b398cd6f520dc536f88d5fadb5458783
ShivamSh08 <113332920+ShivamSh08@users.noreply.github.com>
2024-08-09 13:18:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const mongoose= require('mongoose')
+// const port = 3000
+const houseRouter = require('../Real_Backend/routes/houseroute')
+const enquiryRouter = require('../Real_Backend/routes/enquiryroutes')
+const userRouter = require('../Real_Backend/routes/userroute')
+const dotenv = require('dotenv')
+dotenv.config()
+const port = process.env.PORT
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+
+// const db = module.exports = async()=> {
+//     try{
+//         await mongoose.connect("mongodb://192.168.100.177:27017/",)
+//         console.log("MongoDB Connection is successful")
+//     }catch(e){
+//         console.log(e);
+//         console.log("MongoDB Connection is not successful")
+//     }
+// }
+// db();
+
+
+const db = module.exports = async()=> {
+    try{
+        await mongoose.connect(process.env.MONGOBDURL,{
+            user : process.env.DBUSER,
+            pass : process.env.DBPASS
+        })
+        console.log("MongoDB Connection is successful")
+    }catch(e){
+        console.log(e);
+        console.log("MongoDB Connection is not successful")
+    }
+}
+db();
+
+app.use("/house/",houseRouter)
+app.use("/enquiry/",enquiryRouter)
+app.use("/user/",userRouter)
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
